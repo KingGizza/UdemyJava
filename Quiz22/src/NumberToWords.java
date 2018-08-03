b78a18634e6ee207713b6395eb0a07e319731947
Maaike Rietdijk <maaikerietdijk@gmail.com>
2018-08-03 16:35:55 +0200

    Quiz 22 Done!

--- Quiz22/src/NumberToWords.java
+++ Quiz22/src/NumberToWords.java
@@ -13,67 +13,75 @@ public class NumberToWords {
         numberToWords(1000);
         numberToWords(-12);
     }
+
     public static void numberToWords(int num) {
-        int reverseNumber=reverse(num);
-        while (reverseNumber!=0){
-            int finalNumber=reverseNumber%10;
-            switch (finalNumber){
-                case 1:
-                    System.out.println("One");
-                    break;
-                case 2:
-                    System.out.println("Two");
-                    break;
-                case 3:
-                    System.out.println("Three");
-                    break;
-                case 4:
-                    System.out.println("Four");
-                    break;
-                case 5:
-                    System.out.println("Five");
-                    break;
-                case 6:
-                    System.out.println("Six");
-                    break;
-                case 7:
-                    System.out.println("Seven");
-                    break;
-                case 8:
-                    System.out.println("Eight");
-                    break;
-                case 9:
-                    System.out.println("Nine");
-                    break;
-                case 0:
-                    System.out.println("Zero");
-                    break;
+        int reverseNumber = reverse(num);
+        if (num < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int digitCount=getDigitCount(num);
+            for( int i=0; i<digitCount; i++){
+                int finalNumber = reverseNumber % 10;
+                switch (finalNumber) {
+                    case 1:
+                        System.out.println("One");
+                        break;
+                    case 2:
+                        System.out.println("Two");
+                        break;
+                    case 3:
+                        System.out.println("Three");
+                        break;
+                    case 4:
+                        System.out.println("Four");
+                        break;
+                    case 5:
+                        System.out.println("Five");
+                        break;
+                    case 6:
+                        System.out.println("Six");
+                        break;
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                    case 0:
+                        System.out.println("Zero");
+                        break;
+                }
+                reverseNumber = reverseNumber / 10;
             }
-            reverseNumber=reverseNumber/10;
         }
     }
-    public static int reverse (int originalNumber){
+
+    public static int reverse(int originalNumber) {
         int newNumber = 0;
-        while (originalNumber!=0){
-            newNumber=newNumber+originalNumber%10;
-            originalNumber=originalNumber/10;
-            newNumber=newNumber*10;
+        while (originalNumber != 0) {
+            newNumber = newNumber + originalNumber % 10;
+            originalNumber = originalNumber / 10;
+            newNumber = newNumber * 10;
         }
+        newNumber=newNumber/10;
         return newNumber;
     }
-    public static int getDigitCount(int originalNumber){
+
+    public static int getDigitCount(int originalNumber) {
         int digitCount = 0;
-        if (originalNumber==0){
+        if (originalNumber == 0) {
             return 1;
-        }
-        else if(originalNumber<0){
+        } else if (originalNumber < 0) {
             return -1;
+        } else {
+            while (originalNumber > 0) {
+                digitCount++;
+                originalNumber = originalNumber / 10;
+            }
+            return digitCount;
         }
-        else{
-        while (originalNumber>0){
-            digitCount++;
-            originalNumber=originalNumber/10;
-        }
-        return digitCount;
-    }}
+    }
 }
